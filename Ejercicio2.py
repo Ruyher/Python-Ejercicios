3ed253cc9d7abc047025bc28d79ed753c0065a69
Ruyher <joaquinmanuelmiravet@gmail.com>
2024-05-06 17:25:09 -0300

    Create Ejercicio2.py

---
+++ Ejercicio2.py
@@ -0,0 +1,10 @@
+val = [True, 5, False, "hola", "adios", 2]
+if len(val[3]) < len(val[4]):
+    print("adiós es mayor que hola.")
+else:
+    print("hola es mayor que adiós.")
+
+print(val[2]+val[5])
+print(val[2]-val[5])
+print(val[2]*val[5])
+print(val[2]/val[5])
